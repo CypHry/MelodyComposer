d6e2238e3bfdc2fa4a2e70106d7573ce1971f19d
CypHry <cyprianhryniuk@gmail.com>
2019-09-30 20:05:56 +0200

    comment added

--- utils/splitABC.sh
+++ utils/splitABC.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+## splitABC.sh - Spliting huge ABC files utility
+##
+## This script splits one huge file full of ABC melodies.
+## Each created during split file contains one melody.
+## USAGE: ./splitABC.sh fileToSplit fileToSplit2 ...
+## All passed filenames are used. Passing no arguments has no effect.
+
 if [ $# -eq 0 ];
 then printf "No file passed as argument.\n";
      printf "\n"
-     printf "USAGE: ./splitABC.sh fileToSplit\n"
+     printf "USAGE: ./splitABC.sh fileToSplit fileToSplit2 ...\n"
 fi
