3b5e3a4af552bd5a3f0914327c97bcf18dfa4135
CypHry <cyprianhryniuk@gmail.com>
2019-10-02 09:29:17 +0200

    FileHandleFactory initial implementation.

---
+++ code/file/FileHandleFactory.py
@@ -0,0 +1,30 @@
+## @file FileHandleFactory.py
+#   This file implements factory for creating FileHandles in a safe way.
+
+from enum import Enum
+from FileHandle import WriteOnlyFileHandle, ReadOnlyFileHandle
+import os.path
+
+## @class FileHandleFactory
+#   Implements safe way of creating FileHandles.
+class FileHandleFactory:
+    class FileMode(Enum):
+        ReadOnly = "r"
+        WriteOnly = "w+"
+
+    def createFileHandle(fileMode, fileName):
+        cases = {
+            FileHandleFactory.FileMode.ReadOnly: FileHandleFactory._createReadOnlyFileHandle,
+            FileHandleFactory.FileMode.WriteOnly: FileHandleFactory._createWriteOnlyFileHandle
+        }
+
+        return cases.get(fileMode, None)(fileName)
+
+    def _createReadOnlyFileHandle(fileName):
+        if os.path.isfile(fileName):
+            return ReadOnlyFileHandle(fileName)
+        else:
+            return None
+
+    def _createWriteOnlyFileHandle(fileName):
+        return WriteOnlyFileHandle(fileName)
