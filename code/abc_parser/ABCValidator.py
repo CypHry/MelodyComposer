d0ab3114e8977a84b79cf2326b1d3a8bea4b0ef9
CypHry <cyprianhryniuk@gmail.com>
2019-10-03 21:06:39 +0200

    ABCSplitter class implementation

--- code/abc_parser/ABCValidator.py
+++ code/abc_parser/ABCValidator.py
@@ -2,9 +2,22 @@
 #   Implementation of abc file validator.
 
 from FileHandle import ReadOnlyFileHandle
+from ABCSplitter import ABCSplitter
 
 ## @class ABCValidator
 #   ABC file validator.
 class ABCValidator:
-    def validate(fileHandle):
-        return False
+    def validate(self, fileHandle):
+        infoString, abcString = ABCSplitter.split(fileHandle)
+
+        if not _checkInfo(infoString) or not _checkABC(abcString):
+            return False
+        return True
+
+    # TODO: Converter needs to be implemented first
+    #   Currently I dont know which information is actually needed
+    def _checkInfo(self):
+        return True
+
+    def _checkABC(self):
+        return True
