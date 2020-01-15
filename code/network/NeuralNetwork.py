1ad669f85c12b844c9d2fa6d41118bd07a873a4c
CypHry <cyprianhryniuk@gmail.com>
2020-01-15 17:42:12 +0100

    NN update

--- code/network/NeuralNetwork.py
+++ code/network/NeuralNetwork.py
@@ -8,14 +8,14 @@ class NeuralNetwork:
 
     def __init__(self, input_shape):
         self.model = Sequential()
-        self.model.add(LSTM(256, input_shape=(input_shape[1], input_shape[2]), return_sequences=True))
-        self.model.add(LSTM(256))
+        self.model.add(LSTM(450, input_shape=(input_shape[1], input_shape[2]), return_sequences=True))
+        self.model.add(LSTM(450))
         self.model.add(Dense(input_shape[2]))
         self.model.add(Activation('softmax'))
         self.model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
 
     def train(self, input, output, filepath):
-        self.model.fit(input, output, epochs=100, batch_size=10, verbose=2)
+        self.model.fit(input, output, epochs=100, batch_size=15, verbose=2)
         self.model.save(filepath)
 
     def generate(self, starter):
